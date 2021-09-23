bfc5bf1f1bce6a363fcd5650e79a98f1998cc4a8
SupersLWF <49940288+SupersLWF@users.noreply.github.com>
2021-09-23 20:54:30 +0800

    Update test_1.cpp

--- test_1.cpp
+++ test_1.cpp
@@ -24,7 +24,7 @@ const char *fragmentShaderSource = "#version 330 core\n"
     "out vec4 FragColor;\n"
     "void main()\n"
     "{\n"
-    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"  //直接为片元赋予一个 rgba 颜色值。
     "}\n\0";
 
 
